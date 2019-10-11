b024201f869a79896fc9607a112eed798d5c0512
lesliecdubs <leslie@netlify.com>
2019-10-10 21:50:11 -0500

    Replace text-heavy slides with video and add final slide

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import LayoutBlank from "./layout-blank";
+import LayoutDark from "./layout-dark";
 import LayoutGradientBg from "./layout-gradient-bg";
 import Layout from "./layout";
 import Theme from "./theme";
 import webpackRules from "./webpackRules";
 
-export { LayoutBlank, LayoutGradientBg, Layout, webpackRules };
+export { LayoutBlank, LayoutDark, LayoutGradientBg, Layout, webpackRules };
 
 export default Theme;
